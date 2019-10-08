e36fca7416252d1eaa3fd3cbc10da29f12ec1d02
Jason Maurer <jason@maur.co>
2019-10-07 19:45:06 -0600

    Code from guided lecture

--- src/App.js
+++ src/App.js
@@ -1,82 +1,40 @@
 import "./App.css";
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from "react";
+import { useLocalStorage } from "./utils/input";
+import { useDogImages } from "./utils/api";
 
-class App extends Component {
-  constructor() {
-    super()
+function App(props) {
+  // we're calling our custom hooks here, and passing the parameters we defined in input.js
+  const [breed, setBreed] = useLocalStorage("breed", "husky");
+  const [count, setCount] = useLocalStorage("count", 1);
+  // we don't need setImages anymore, since we never call it
+  const [images] = useDogImages(breed, count);
 
-    // set some initial/default state values
-    this.state = {
-      breed: 'husky',
-      images: []
-    }
-  }
+  return (
+    <>
+      <h1>The Dog Website</h1>
 
-  componentDidMount() {
-    // fetch images for the default state value (husky)
-    this.fetchDogImages()
-  }
+      <select value={breed} onChange={e => setBreed(e.target.value)}>
+        <option value="husky">Husky</option>
+        <option value="beagle">Beagle</option>
+        <option value="corgi">Corgi</option>
+        <option value="boxer">Boxer</option>
+      </select>
 
-  componentDidUpdate(prevProps, prevState) {
-    // this if statement prevents an infinite loop
-    // by only re-fetching if the breed has changed since the last update
-    //
-    // ----- same idea -----
-    //   useEffect(() => {
-    // 
-    //   }, [breed])
-    //
-    if (prevState.breed !== this.state.breed) {
-      // give the user feedback that something is happening
-      // by clearing out the current images while fetching new ones
-      this.setState({ images: [] })
+      <input
+        type="number"
+        placeholder="Image Count"
+        value={count}
+        onChange={e => setCount(e.target.value)}
+      />
 
-      // re-fetch images with new state value
-      this.fetchDogImages()
-    }
-  }
-
-  // extract this function from lifecycle event since it's used multiple times
-  fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
-      .then(result => {
-        this.setState({
-          images: result.data.message
-        })
-      })
-      .catch(error => {
-        console.log('error:', error)
-      })
-  }
-
-  // make our select field controlled by react state
-  handleChange = (event) => {
-    this.setState({
-      breed: event.target.value
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <h1>The Dog Website</h1>
-
-        <select value={this.state.breed} onChange={this.handleChange}>
-          <option value="husky">Husky</option>
-          <option value="beagle">Beagle</option>
-          <option value="corgi">Corgi</option>
-          <option value="boxer">Boxer</option>
-        </select>
-
-        <div>
-          {this.state.images.map((image, index) => (
-            <img key={index} src={image} alt="Dog" />
-          ))}
-        </div>
-      </>
-    )
-  }
+      <div>
+        {images.map((image, index) => (
+          <img key={index} src={image} alt="Dog" />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
